3c3c39e362f16926beec6235837762f8ba4c0d91
Kishor Bhagat <89726870+KishorBhagat@users.noreply.github.com>
2022-06-14 00:39:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,40 @@
+window.onscroll = function(){
+    scrollFunction();
+};
+let upBtn = document.getElementById('upBtn');
+function scrollFunction(){
+    if(document.body.scrollTop > 20 || document.documentElement.scrollTop > 20){
+        upBtn.style.display = "block";
+    }
+    else{
+        upBtn.style.display = "none";
+    }
+}
+
 
 let menuIcon = document.getElementById('menu-icon');
+let crossIcon = document.getElementById('cross-icon');
 let menuList = document.getElementById('menuList');
 
 menuIcon.addEventListener('click', toggle);
+crossIcon.addEventListener('click', toggle);
 let visibility = false;
 function toggle(){
     // console.log("clicked");
     if(visibility == false){
+
+        crossIcon.style.display = "block";
+        menuIcon.style.display = "none";
         menuList.style.visibility = "visible";
         visibility = true;
+        document.body.style.overflow = "hidden"
     }
     else{
+        crossIcon.style.display = "none";
+        menuIcon.style.display = "block";
         menuList.style.visibility = "hidden";
         visibility = false;
+        document.body.style.overflow = "visible"
     }
 }
 
@@ -25,11 +47,12 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     })
 })
 
-
 let menuLinks = document.getElementsByClassName('menuLinks');
 for(let link of menuLinks){
     link.addEventListener('click', function close(){
         menuList.style.visibility = "hidden";
         visibility = false;       
+        
+        document.body.style.overflow = "visible"
     })
 }
