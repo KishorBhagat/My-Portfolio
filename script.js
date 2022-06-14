4f70859993a74cc10bcf2b84b2281ea1b7f49892
Kishor Bhagat <89726870+KishorBhagat@users.noreply.github.com>
2022-06-15 02:31:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -12,32 +12,7 @@ function scrollFunction(){
 }
 
 
-let menuIcon = document.getElementById('menu-icon');
-let crossIcon = document.getElementById('cross-icon');
-let menuList = document.getElementById('menuList');
-
-menuIcon.addEventListener('click', toggle);
-crossIcon.addEventListener('click', toggle);
-let visibility = false;
-function toggle(){
-    // console.log("clicked");
-    if(visibility == false){
-
-        crossIcon.style.display = "block";
-        menuIcon.style.display = "none";
-        menuList.style.visibility = "visible";
-        visibility = true;
-        document.body.style.overflow = "hidden"
-    }
-    else{
-        crossIcon.style.display = "none";
-        menuIcon.style.display = "block";
-        menuList.style.visibility = "hidden";
-        visibility = false;
-        document.body.style.overflow = "visible"
-    }
-}
-
+// Code to smothely navigate to sections when nav links are clicked
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function(e){
         e.preventDefault();
@@ -47,10 +22,41 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     })
 })
 
+let menuIcon = document.getElementById('menu-icon');
+let crossIcon = document.getElementById('cross-icon');
+let verticalmenuList = document.getElementById('verticalmenuList');
+
+menuIcon.addEventListener('click', toggle);
+crossIcon.addEventListener('click', toggle);
+let visibility = false;
+function toggle(){
+    // console.log("opened");
+    if(visibility == false){
+
+        crossIcon.style.display = "block";
+        menuIcon.style.display = "none";
+        verticalmenuList.style.visibility = "visible";
+        visibility = true;
+        document.body.style.overflow = "hidden"
+    }
+    else{
+        // console.log("closed");
+        crossIcon.style.display = "none";
+        menuIcon.style.display = "block";
+        verticalmenuList.style.visibility = "hidden";
+        visibility = false;
+        document.body.style.overflow = "visible"
+    }
+}
+
+
+// Code for what to do when the vertical menu bar navigation links are clicked
 let menuLinks = document.getElementsByClassName('menuLinks');
 for(let link of menuLinks){
     link.addEventListener('click', function close(){
-        menuList.style.visibility = "hidden";
+        crossIcon.style.display = "none";
+        menuIcon.style.display = "block";
+        verticalmenuList.style.visibility = "hidden";
         visibility = false;       
         
         document.body.style.overflow = "visible"
